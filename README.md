a3c9ab2eaeaa277c4d283be3be6015a0833a0bd0
ChrisLuo <313963196@qq.com>
2018-04-19 00:47:26 +0800

    五子棋最终版
    新增切换版本功能

--- README.md
+++ README.md
@@ -1,4 +1,2 @@
 # gobang
-五子棋dom版
-应腾讯CDC面试官bobb邀请完成
-下载之后谷歌浏览器打开gobang.html即可使用
+应腾讯CDC面试官bobb邀请，完成五子棋，允许Dom版和Canvas版切换，可悔棋，可撤销悔棋
--- gobang.js
+++ gobang.js
@@ -2,33 +2,82 @@
  * 罗运来 五子棋 dom版
  */
 window.onload = () => {
+    let dom = document.getElementById('dom')
+    let canvas = document.getElementById('canvas')
     let table = document.getElementById('table')
+    let table1 = document.getElementById('table1')
+    // ctx = table1.getContext('2d')
     loadTable()
 }
-
+edition = 'dom' //当前版本
 temp = null //悔的那步棋
+tempCanvas={
+    img:null,
+    chess:null
+}
 backTime = 0 //悔棋次数
 chessArr = [] //棋子数组
+initState=null
+canvasStates = [] //保存canvas状态
+/**
+ * 改版本
+ */
+function change() {
+    if (edition == 'canvas') {
+        edition = 'dom'
+        restartC()
+    } else {
+        edition = 'canvas'
+        restart()
+    }
+    loadTable()
+}
 /**
  * 绘制棋盘
  */
 function loadTable() {
-    for (var i = 0; i < 15; i++) {
-        let line = document.createElement('div')
-        for (var j = 0; j < 15; j++) {
-            line.innerHTML += "<div class='box'></div>"
+    if (edition != 'dom') {
+        dom.style.display = 'none'
+        canvas.style.display = 'flex'
+        table1.height = table1.height
+        let ctx = table1.getContext('2d')
+        for (var i = 0; i <= 15; i++) {
+            draw(ctx, 10, i*40+10, 610, i*40+10)
+            draw(ctx, i*40+10, 10, i*40+10, 610)
         }
-        table.appendChild(line)
+        ctx.stroke()
+        initState=ctx.getImageData(0, 0, 620, 620)
+        canvasStates.push(initState) //保存棋盘状态
+        canvas.addEventListener('click', a, true)
+    } else {
+        canvas.style.display = 'none'
+        dom.style.display = 'flex'
+        for (var i = 0; i < 15; i++) {
+            let line = document.createElement('div')
+            for (var j = 0; j < 15; j++) {
+                line.innerHTML += "<div class='box'></div>"
+            }
+            table.appendChild(line)
+        }
+        table.addEventListener('click', a, true) //事件委托
     }
-    table.addEventListener('click', a, true)//事件委托
+}
+/**
+ * 画棋盘线
+ */
+function draw(ctx, x, y, x1, y1) {
+    ctx.moveTo(x, y)
+    ctx.lineTo(x1, y1)
+    ctx.lineWidth = '1.1'
 }
 
 function a(event) { //用于绑定和解绑
-    create(event.clientX, event.clientY)
+    edition == 'dom' ?
+        create(event.clientX, event.clientY) : createC(event.offsetX-10, event.offsetY-10)
 }
 
 /**
- * 下棋
+ * 下棋 - dom
  */
 function create(x, y) {
     var chess = document.createElement('img')
@@ -36,7 +85,7 @@ function create(x, y) {
     chess.style.position = 'absolute'
     var _x = x - table.offsetLeft
     var _y = y - table.offsetTop
-    if (_x % 40 >= 5 && _x % 40 <= 35 || _y % 40 >= 5 && _y % 40 <= 35) {//不在半径范围内
+    if (_x % 40 >= 5 && _x % 40 <= 35 || _y % 40 >= 5 && _y % 40 <= 35) { //不在半径范围内
         return
     } else {
         let chessX = table.offsetLeft + Math.round(_x / 40) * 40 - 10.5 //获取准确left,top值以定位棋子
@@ -50,21 +99,62 @@ function create(x, y) {
             y: chessY,
             type: chessArr.length % 2 == 0 ? 'black' : 'white'
         })
-        if (chessArr.length >= 9) {//棋子最少要9颗才能赢
+        if (chessArr.length >= 9) { //棋子最少要9颗才能赢
             var a = ifWin()
-            if (a){
+            if (a) {
                 setTimeout(() => {
                     alert(a)
                 }, 0);
-                chessArr=[]
-                temp=null
+                chessArr = []
+                temp = null
             }
         }
     }
     backTime = 0 //过棋之后才能悔棋
 }
 /**
- * 重开
+ * 下棋 - canvas
+ */
+function createC(x, y) {
+    if (x % 40 >= 5 && x % 40 <= 35 || y % 40 >= 5 && y % 40 <= 35) { //不在半径范围内
+        return
+    }else {
+        let ctx = table1.getContext('2d')
+        let chessX = Math.round(x / 40) * 40 + 10
+        let chessY = Math.round(y / 40) * 40 + 10
+        if(chessArr.filter(item=>item.x==chessX&&item.y==chessY).length!=0) return
+        canvasStates.push(ctx.getImageData(0, 0, 620, 620)) //保存下棋前的状态
+        ctx.beginPath()
+        ctx.arc(chessX, chessY, 10, 0, 2 * Math.PI)
+        chessArr.length % 2 == 0 ? ctx.fillStyle = 'black' : ctx.fillStyle = 'white'
+        var obj = {
+            x: chessX,
+            y: chessY,
+            type: chessArr.length % 2 == 0 ? 'black' : 'white'
+        }
+        chessArr.push(obj)
+        ctx.fill()
+        ctx.closePath()
+        tempCanvas.img = ctx.getImageData(0, 0, 620, 620) //保存下棋后的状态
+        tempCanvas.chess = obj
+        if (chessArr.length >= 9) { //棋子最少要9颗才能赢
+            var a = ifWin()
+            if (a) {
+                setTimeout(() => {
+                    alert(a)
+                }, 0);
+                chessArr = []
+                tempCanvas={
+                    img:null,
+                    chess:null
+                }
+            }
+        }
+    }
+    backTime = 0 //过棋之后才能悔棋
+}
+/**
+ * 重开 - dom
  */
 function restart() {
     Array.from(document.querySelectorAll('.chess')).forEach(item => document.body.removeChild(item))
@@ -75,17 +165,37 @@ function restart() {
 }
 
 /**
- * 悔棋
+ * 重开 -canvas
  */
-function back() {
+function restartC() {
+    let ctx = table1.getContext('2d')
+    ctx.putImageData(initState, 0, 0)
+    chessArr = []
+    tempCanvas={
+        img:null,
+        chess:null
+    }
+    canvasStates = []
+    backTime = 0
+    canvas.addEventListener('click', a, true)
+}
+/**
+ * 悔棋 - dom
+ */
+function back(event) {
     if (chessArr.length == 0) {
         return
     }
     if (backTime != 0) {
         alert('你已经悔过棋了！')
     } else {
-        temp = Array.from(document.querySelectorAll('.chess')).pop()
-        document.body.removeChild(temp)
+        if (event.target.id == 'dom-back') {
+            temp = Array.from(document.querySelectorAll('.chess')).pop()
+            document.body.removeChild(temp)
+        } else { //canvas
+            let ctx = table1.getContext('2d')
+            ctx.putImageData(canvasStates.pop(), 0, 0)
+        }
         chessArr.pop()
         backTime++
     }
@@ -102,6 +212,22 @@ function cancelBack() {
             type: temp.src.includes('black') ? 'black' : 'white'
         })
         temp = null
+    } else {
+        alert('没有悔棋哦！')
+    }
+}
+/**
+ * 
+ */
+function cancelBackC(params) {
+    if (tempCanvas.chess != null) {
+        let ctx = table1.getContext('2d')
+        ctx.putImageData(tempCanvas.img, 0, 0)
+        chessArr.push(tempCanvas.chess)
+        tempCanvas={
+            img:null,
+            chess:null
+        }
     } else {
         alert('没有悔棋哦！')
     }
@@ -134,7 +260,7 @@ function ifWin() {
             }
         }
         if (leftRightCount == 5) {
-            table.removeEventListener('click', a, true)
+            edition=='dom'?table.removeEventListener('click', a, true):canvas.removeEventListener('click',a,true)
             return lastChess.type == 'black' ? '黑棋胜利' : '白棋胜利'
         }
     }
@@ -156,7 +282,7 @@ function ifWin() {
             }
         }
         if (upDownCount == 5) {
-            table.removeEventListener('click', a, true)
+            edition=='dom'?table.removeEventListener('click', a, true):canvas.removeEventListener('click',a,true)
             return lastChess.type == 'black' ? '黑棋胜利' : '白棋胜利'
         }
     }
@@ -178,7 +304,7 @@ function ifWin() {
             }
         }
         if (angCount == 5) {
-            table.removeEventListener('click', a, true)
+            edition=='dom'?table.removeEventListener('click', a, true):canvas.removeEventListener('click',a,true)
             return lastChess.type == 'black' ? '黑棋胜利' : '白棋胜利'
         }
     }
